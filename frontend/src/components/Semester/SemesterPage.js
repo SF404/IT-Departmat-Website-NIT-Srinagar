a7428f5282026da2b864f0fafcfdae26b896b787
Suhaib <enigma00800@gmail.com>
2023-10-12 13:25:25 +0530

    Updated Semester Page

--- frontend/src/components/Semester/SemesterPage.js
+++ frontend/src/components/Semester/SemesterPage.js
@@ -1,10 +1,12 @@
 // SemesterPage.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Heading, Text, List, ListItem, Button, HStack, Divider, Flex, TabIndicator, UnorderedList, Badge, Spinner } from '@chakra-ui/react';
+import { Box, Heading, Text, List, ListItem, Button, HStack, Divider, Flex, TabIndicator, UnorderedList, Tag, Badge, Spinner, VStack, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure } from '@chakra-ui/react';
 import { Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import Footer from '../Layout/Footer';
+import axios from 'axios';
+
 
 const SemesterPage = () => {
     const { semesterId } = useParams();
@@ -17,13 +19,13 @@ const SemesterPage = () => {
             details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             subjects: [
                 { id: 1, credit: 4, name: 'Computer Organization and Architecture', courseId: 'ITT303', instructor: 'Instructor 1' },
-                { id: 2, credit: 4, name: 'Data Communication', courseId: 'ITT303', instructor: 'Instructor 2' },
-                { id: 2, credit: 4, name: 'Design And Analysis of Algorithm', courseId: 'ITT303', instructor: 'Instructor 3' },
-                { id: 2, credit: 4, name: 'Microprocessor', courseId: 'ITT303', instructor: 'Instructor 4' },
-                { id: 2, credit: 4, name: 'Theory of Computation', courseId: 'ITT303', instructor: 'Instructor 5' },
-                { id: 2, credit: 3, name: 'Introduction to probability and Statistics', courseId: 'ITT303', instructor: 'Instructor 6' },
-                { id: 2, credit: 1, name: 'Design And Analysis of Algorithm Lab', courseId: 'ITT303', instructor: 'Instructor 7' },
-                { id: 2, credit: 1, name: 'Microprocessor Lab', courseId: 'ITT303', instructor: 'Instructor 7' },
+                { id: 2, credit: 4, name: 'Data Communication', courseId: 'ITT304', instructor: 'Instructor 2' },
+                { id: 2, credit: 4, name: 'Design And Analysis of Algorithm', courseId: 'ITT305', instructor: 'Instructor 3' },
+                { id: 2, credit: 4, name: 'Microprocessor', courseId: 'ITT306', instructor: 'Instructor 4' },
+                { id: 2, credit: 4, name: 'Theory of Computation', courseId: 'ITT307', instructor: 'Instructor 5' },
+                { id: 2, credit: 3, name: 'Introduction to probability and Statistics', courseId: 'ITT308', instructor: 'Instructor 6' },
+                { id: 2, credit: 1, name: 'Design And Analysis of Algorithm Lab', courseId: 'ITT309', instructor: 'Instructor 7' },
+                { id: 2, credit: 1, name: 'Microprocessor Lab', courseId: 'ITT310', instructor: 'Instructor 7' },
                 // Add more subjects as needed
             ],
             // Add more properties as needed
@@ -32,44 +34,147 @@ const SemesterPage = () => {
 
     const semester = fetchSemesterData(semesterId);
 
-    // states
+
+    // states 
+    const [notes, setNotes] = useState([])
+    const [assignments, setAssignments] = useState([])
+    const [selectedCourse, setSelectedCourse] = useState([])
     const [loading, setLoading] = useState(true);
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const [details, setDetails] = useState({
+        name: '',
+        details: '',
+        deadline: '',
+    });
+
+    // functions
+    async function fetchAssignments() {
+        try {
+            const assignment = await axios.get(
+                `http://localhost:8000/api/showassignment/?cid=${selectedCourse}`
+            );
+            setAssignments(assignment.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
 
 
-    function TabSection() {
-        return (
-            <>
-                <Tabs  minH={'100px'} position="relative" defaultIndex={0} size='md' variant="unstyled" borderRadius={16} boxShadow={'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'}>
-                    <TabList bg={'green.100'} borderRadius='16px 16px 0 0 '>
-                        <Tab fontWeight={'bold'}>Notes</Tab>
-                        <Tab fontWeight={'bold'}>Assignments</Tab>
-                        <Tab fontWeight={'bold'}>Other</Tab>
-                    </TabList>
-                    <TabIndicator
-                        mt="-1.5px"
-                        height="2px"
-                        bg="#81c784"
-                        borderRadius="1px"
-                    />
-                    {
-                        loading ? (<Spinner />) : (
-                            <TabPanels>
-                                <TabPanel>
-                                    <p>Notes</p>
-                                </TabPanel>
-                                <TabPanel>
-                                    <p>Assignments</p>
-                                </TabPanel>
-                                <TabPanel>
-                                    <p>Other</p>
-                                </TabPanel>
-                            </TabPanels>
-                        )
-                    }
-                </Tabs>
-            </>
-        )
+    async function fetchNotes() {
+        setLoading(true);
+        try {
+            const notes = await axios.get(
+                `http://localhost:8000/api/shownotes/?cid=${selectedCourse}`
+            );
+            setNotes(notes.data);
+            if (notes.status === 200) {
+                setTimeout(() => {
+                    setLoading(false);
+                }, 1000);
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    function handleDownload() {
+
     }
+
+    const showDetails = (name, details, deadline) => {
+        setDetails({ name, details, deadline });
+        onOpen();
+    };
+
+
+
+
+    // use effects
+    useEffect(() => {
+        if (selectedCourse != null) {
+            fetchNotes();
+            fetchAssignments();
+        }
+
+    }, [selectedCourse])
+
+
+    // handlers
+
+
+    // Sub Components
+
+    function DataTabs() {
+        return (<>
+            <Tabs position="relative" defaultIndex={0} size='md' variant="unstyled" borderRadius={16} boxShadow="rgba(100, 100, 111, 0.2) 0px 7px 29px 0px">
+                <TabList bg={'green.100'} color={'green'} borderRadius="16px 16px 0 0" >
+                    <Tab fontWeight={'bold'}>Notes</Tab>
+                    <Tab fontWeight={'bold'}>Assignments</Tab>
+                    <Tab fontWeight={'bold'}>Other</Tab>
+                </TabList>
+                <TabIndicator
+                    mt="-1.5px"
+                    height="2px"
+                    bg="#81c784"
+                    borderRadius="1px"
+                />
+                <TabPanels minH={100}>
+                    <TabPanel >
+
+                        <VStack spacing={2}>
+                            {
+                                loading ? (<Spinner size="lg" />) :
+                                    notes.map((item) => (
+                                        <Flex w={'full'} key={item.id} justifyContent={'space-between'} alignItems={'center'} gap={4}>
+                                            <Text>{item.name}</Text>
+                                            <Button onClick={() => handleDownload(item.id)}>Download</Button>
+                                        </Flex>
+                                    ))
+                            }
+                        </VStack>
+                    </TabPanel>
+                    <TabPanel>
+                        <VStack spacing={2}>
+                            {
+                                loading ? (<Spinner size="lg" />) :
+                                    assignments.map((item) => (
+                                        <Flex w={'full'} key={item.id} justifyContent={'space-between'} alignItems={'center'} gap={4}>
+                                            <Text >{item.name}</Text>
+
+                                            <Badge>Due: <span style={{ color: 'red' }}>{item.deadline}</span></Badge>
+                                            <Button onClick={() => { showDetails(item.name, item.details, item.deadline); onOpen() }}>Details</Button>
+                                            <Button onClick={() => handleDownload(item.id)}>Download</Button>
+                                        </Flex>
+                                    ))
+                            }
+                            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                                <ModalOverlay />
+                                <ModalContent>
+                                    <ModalHeader>Create your account</ModalHeader>
+                                    <ModalCloseButton />
+                                    <ModalBody pb={6}>
+
+                                    </ModalBody>
+
+                                    <ModalFooter>
+                                        <Button colorScheme='blue' mr={3}>
+                                            Save
+                                        </Button>
+                                        <Button onClick={onClose}>Cancel</Button>
+                                    </ModalFooter>
+                                </ModalContent>
+                            </Modal>
+                        </VStack>
+                    </TabPanel>
+                    <TabPanel>
+                        <p>Other</p>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </>)
+    }
+
+
 
     return (
         <>
@@ -93,9 +198,9 @@ const SemesterPage = () => {
 
                     {
                         semester.subjects.map((subject) => (
-                            <AccordionItem w={'full'} cursor={'pointer'}>
+                            <AccordionItem key={subject.courseId}>
                                 <h2>
-                                    <AccordionButton _expanded={{ bg: '#009688', color: 'white' }} h={'64px'} as={Badge}>
+                                    <AccordionButton as={Badge} cursor={'pointer'} _expanded={{ bg: "teal", color: "white" }} h={'64px'} onClick={() => { setSelectedCourse(subject.courseId); console.log('clicked') }}>
                                         <Box as="span" flex='1' textAlign='left' fontWeight={'bold'}>
                                             <Flex justifyContent={'space-between'}>
                                                 <Text> {subject.courseId} | {subject.name}</Text>
@@ -104,20 +209,20 @@ const SemesterPage = () => {
                                         <AccordionIcon />
                                     </AccordionButton>
                                 </h2>
-                                <AccordionPanel pb={4}>
-                                    <HStack justifyContent={'space-between'}>
+                                <AccordionPanel pb={4} zz>
+                                    <Box display={'flex'} justifyContent={'space-between'}>
                                         <Box>
                                             <Text>{subject.instructor}</Text>
                                             <Badge colorScheme='green'>Credit: {subject.credit}</Badge>
                                         </Box>
                                         <Text>Syllabus: <Button>Download</Button></Text>
-                                    </HStack>
+                                    </Box>
 
                                     <Divider my={2} />
 
-                                    <TabSection />
-
-
+                                    {
+                                        selectedCourse && <DataTabs />
+                                    }
                                 </AccordionPanel>
                             </AccordionItem>
                         ))
