a7e5f9a93f1a2d83427e6caae29198302d7eb9fe
Rajes <rmrajesofficial.0@gmail.com>
2023-10-26 14:26:41 +0530

    updated

--- IT_Department_NIT_Srinagar/settings.py
+++ IT_Department_NIT_Srinagar/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
