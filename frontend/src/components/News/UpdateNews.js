305103dc40a99ed6867b4ddb3e68bec4ce98f6a5
Suhaib <enigma00800@gmail.com>
2023-11-23 14:39:35 +0530

    Updated

--- frontend/src/components/News/UpdateNews.js
+++ frontend/src/components/News/UpdateNews.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Box, Button, Center, FormControl, FormLabel, Input, Textarea, VStack } from '@chakra-ui/react';
+import { Box, Button, Center, FormControl, FormLabel, Input, SimpleGrid, Textarea, VStack } from '@chakra-ui/react';
 
 const UpdateNews = () => {
   const [formData, setFormData] = useState({
     headline: '',
-    author: '',
     date: '',
     content: '',
     image: null,
@@ -50,40 +49,46 @@ const UpdateNews = () => {
         content: '',
         image: null,
       });
-
-      // Add additional logic for success, such as displaying a success message
     } catch (error) {
       console.error('Error adding news:', error);
-      // Handle error, e.g., display an error message
     }
   };
 
   return (
-    <Center  p="4">
-      <VStack spacing="4" p={4} width={{base:'100%', md:'50%'}} bg={'white'}>
-        <FormControl>
-          <FormLabel display={'flex'} justifyContent={'center'} alignItems={'center'} bg={'rgba(0,0,0,0.1)'} fontWeight={'bold'} color={'rgba(0,0,0,0.6)'} border={'3px dashed rgba(0,0,0,0.3)'} padding={4} height={'100px'} width={'100%'} borderRadius={'12px'}>Image</FormLabel>
-          <Input type="file" name="image" onChange={handleImageChange} display={'none'} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Headline</FormLabel>
-          <Input type="text" name="headline" value={formData.headline} onChange={handleChange} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Content</FormLabel>
-          <Textarea name="content" value={formData.content} onChange={handleChange} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Date</FormLabel>
-          <Input type="date" name="date" value={formData.date} onChange={handleChange} />
-        </FormControl>
-        <Box textAlign={'right'} w={'full'}>
+    <Center p="4" h={'100vh'} w={'full'}>
+      <SimpleGrid columns={[1, 1, 2, 2]} w={{ base: '100%', md: '80%' }} gap={4}>
 
-          <Button colorScheme="teal" type="submit" onClick={handleSubmit}>
-            Publish
-          </Button>
+        <Box bg={'white'} borderRadius={8} w={'full'} h={'full'} boxShadow={'0 0 6px rgba(0,0,0,0.05)'}>
+          <Box h={'40px'} w={'full'} bg={'#99d5cf'} borderTopRadius={8}></Box>
+          <VStack spacing="4" p={8}>
+            <FormControl>
+              <FormLabel display={'flex'} justifyContent={'center'} alignItems={'center'} bg={'rgba(0,0,0,0.1)'} fontWeight={'bold'} color={'rgba(0,0,0,0.6)'} border={'3px dashed rgba(0,0,0,0.3)'} padding={4} height={'100px'} width={'100%'} borderRadius={'12px'}>Image</FormLabel>
+              <Input type="file" name="image" onChange={handleImageChange} display={'none'} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Headline</FormLabel>
+              <Input type="text" name="headline" value={formData.headline} onChange={handleChange} maxLength={'200'} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Content</FormLabel>
+              <Textarea name="content" rows={8} value={formData.content} onChange={handleChange} maxLength={'1000'} />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Date</FormLabel>
+              <Input type="date" name="date" value={formData.date} onChange={handleChange} />
+            </FormControl>
+            <Box textAlign={'right'} w={'full'}>
+
+              <Button colorScheme="teal" type="submit" onClick={handleSubmit}>
+                Publish
+              </Button>
+            </Box>
+          </VStack>
         </Box>
-      </VStack>
+        <VStack bg={'white'} borderRadius={8}>
+
+        </VStack>
+      </SimpleGrid>
     </Center>
   );
 };
